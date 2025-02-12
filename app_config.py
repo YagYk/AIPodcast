a38625f646e34a971d4c09b0076758a31336e2c9
Yagyansh Khandelwal <yagyanshk.33@gmail.com>
2025-02-12 20:00:00 +0530

    Your commit message

--- app_config.py
+++ app_config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+"""api keys"""
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
 OPENAI_DEPLOYMENT_NAME = os.getenv("OPENAI_DEPLOYMENT_NAME")
